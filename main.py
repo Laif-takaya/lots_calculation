b67502dd82d2e5e6d3b5c0e4ad5bbac2657206cc
Laif-takaya <prayingrun1001@gmail.com>
2019-09-30 23:15:44 +0900

    Webhook error修正ver3

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ import os
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = "KMVTxjxp7pxbUHk9Xb4uL0v8/1rDvOJUOfAR5tp18ImgWBJn349FApfcqyR2xmStAYoabAyfTpcAv5AyJAF0Fq+sFJKddUVgPo+cSbXk3YucNLlYHXEt+wd+0BngdGyWcKUXMXkDcjYFDWYS9J1zfAdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET = "51eaf0af063d62d67140f01297f1b310"
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -40,9 +40,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.reply_token == "00000000000000000000000000000000":
-        return
-
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
