8f748383cdf4e73639fc7df3b22eb3aec118d7b6
Laif-takaya <prayingrun1001@gmail.com>
2019-10-02 08:15:07 +0900

    first commit1

--- lots_calculation.py
+++ lots_calculation.py
@@ -14,8 +14,8 @@ import os
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = "KMVTxjxp7pxbUHk9Xb4uL0v8/1rDvOJUOfAR5tp18ImgWBJn349FApfcqyR2xmStAYoabAyfTpcAv5AyJAF0Fq+sFJKddUVgPo+cSbXk3YucNLlYHXEt+wd+0BngdGyWcKUXMXkDcjYFDWYS9J1zfAdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET = "51eaf0af063d62d67140f01297f1b310"
+YOUR_CHANNEL_ACCESS_TOKEN = "WS+Wolw2SG+oGZMBaJOOZyITQTNfi0321I+JHnZ0jeT/528+8do4AOtGjdvJyGL8vhIau7xlt0AtzJT0jVEuLhKDndFf+9kY23/yElYFd6aBKM3UoLu/Kt/iuB9qCb1Ho34idLyPDCBBsKELeqVirQdB04t89/1O/w1cDnyilFU="
+YOUR_CHANNEL_SECRET = "82870d88cfe62a8068a12b66ac294a6d"
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -42,37 +42,11 @@ def callback():
 def handle_message(event):
     req_message=event.message.text
 
-    currency_pair=req_message[:7]
-    stop=req_message[0:2]
-    up_down=req_message[-2:]
-    if currency_pair=="USD/JPY":
-          a="ドル円"
-    elif currency_pair=="EUR/JPY":
-          a="ユーロ円"
-    elif currency_pair=="EUR/USD":
-          a="ユーロドル"
-    elif currency_pair=="AUD/JPY":
-          a="豪円"
-    elif currency_pair=="AUD/USD":
-          a="豪ドル"
-    elif currency_pair=="GBP/JPY":
-          a="ポンド円"
-    elif currency_pair=="NZD/JPY":
-          a="ニュージランド円"
-    else:
-          a="通過ペアは存在しません"
+    #currency_pair=req_message[:7]
 
-    if up_down=="ハイ":
-          c="上"
-    elif up_down=="ロー":
-          c="下"
-    else:
-          c="不明"
-
-    text_back=a+c
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=text_back))
+        TextSendMessage(text=req_message))
 
 
 if __name__ == "__main__":
