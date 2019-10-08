e2a871d54bf91329351da201afe16279d82d94f9
Laif-takaya <prayingrun1001@gmail.com>
2019-10-09 02:19:07 +0900

    新しいコミット

---
+++ scraping.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://info.finance.yahoo.co.jp/fx/list/"
+def getNews(word):
+
+    HTML = requests.get(URL)
+    SOUP = BeautifulSoup(HTML.content, "html.parser")
+    rm=word.split()
+
+    currency_pair=rm[0]
+    account_balance=rm[1]
+    difference=rm[2]
+    ab=float(account_balance)
+    df=float(difference)
+
+    if "JPY" in currency_pair:
+        cp=100
+    elif "GBP/USD" == currency_pair:
+        RES = SOUP.find(id="GBPJPY_chart_ask")
+        RES=RES.string
+        cp=float(RES)
+    elif "AUD/USD" == currency_pair:
+        RES = SOUP.find(id="AUDJPY_chart_ask")
+        RES=RES.string
+        cp=float(RES)
+    elif "EUR/GBP" == currency_pair:
+        RES = SOUP.find(id="EURJPY_chart_ask")
+        RES=RES.string
+        cp=float(RES)
+    elif "EUR/USD" == currency_pair:
+        RES = SOUP.find(id="EURJPY_chart_ask")
+        RES=RES.string
+        cp=float(RES)
+    elif "NZD/USD" == currency_pair:
+        RES = SOUP.find(id="NZDJPY_chart_ask")
+        RES=RES.string
+        cp=float(RES)
+    lots=ab*0.2/df/cp
+
+    result = lots
+    return result
