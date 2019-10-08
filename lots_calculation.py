2b8e03185b718c17b37ae53e963061629385cb28
Laif-takaya <prayingrun1001@gmail.com>
2019-10-09 02:50:22 +0900

    修正

--- lots_calculation.py
+++ lots_calculation.py
@@ -9,6 +9,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import urllib.request
 import os
 from bs4 import BeautifulSoup
 import scraping as sc
